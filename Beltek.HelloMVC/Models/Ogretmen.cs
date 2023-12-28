94f7f2e4b3d77bb6ca9347f76494ad73c19b192b
helinkarabulut <helinkarabulut9@gmail.com>
2023-12-28 23:41:22 +0300

    Add project files.

---
+++ Beltek.HelloMVC/Models/Ogretmen.cs
@@ -0,0 +1,10 @@
+﻿namespace Beltek.HelloMVC.Models
+{
+    public class Ogretmen
+    {
+        public string Tckimlik { get; set; }//Id Primary Key- Fluent Api 
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Brans { get; set; }
+    }
+}
